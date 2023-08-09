9c91ee682be81cfda895ccb1f89d8d62a9b5556e
Arun Kumar R <aruntutter19@gmail.com>
2023-08-09 16:16:49 +0530

    added edit btn & local storage

--- scripts/script.js
+++ scripts/script.js
@@ -10,12 +10,19 @@ const moneyPlusEl = document.getElementById("money-plus");
 const moneyMinusEl = document.getElementById("money-minus");
 const listContainerElement = document.getElementById("list");
 
+const btnSubmitEl = document.getElementById("btn-submit");
+
 // global variables
 let balance;
 let income;
 let expense;
 
-let transactions = [];
+let transactions = localStorage.getItem("transactions")
+  ? JSON.parse(localStorage.getItem("transactions"))
+  : [];
+
+let isEditing;
+let itemToEdit;
 
 // functions
 const init = () => {
@@ -25,6 +32,14 @@ const init = () => {
   balance = 0;
   income = 0;
   expense = 0;
+
+  isEditing = false;
+  itemToEdit = {};
+
+  // calculate Totals
+  calculateTotals();
+
+  addTransactionToDOM(transactions);
 
   balanceEl.innerHTML = `₹${balance.toFixed(2)}`;
   moneyPlusEl.innerHTML = `₹${income.toFixed(2)}`;
@@ -44,10 +59,10 @@ const addTransactionToDOM = (arr) => {
 
     // add data
 
-    const amountValue = amount > 0 ? `₹${amount}` : `₹-${amount}`;
+    const amountValue = amount > 0 ? `₹${amount}` : `₹-${-amount}`;
     listEl.classList = amount > 0 ? "plus" : "minus";
     listEl.innerHTML = `
-      ${transaction}<span>₹${amount}</span><button class="delete-btn" onClick = deleteTransaction(${id}) >x</button>`;
+      ${transaction}<span>${amountValue}</span><button class="delete-btn" onClick = deleteTransaction(${id})><i class="fa-solid fa-trash"></i></button> <button class="edit-btn" onClick = editTransaction(${id})><i class="fa-solid fa-pen-to-square"></i></button>`;
     // appendChild
     listContainerElement.appendChild(listEl);
   });
@@ -57,10 +72,27 @@ const deleteTransaction = (id) => {
   // filter method - filter all the elements expect the ID
   transactions = transactions.filter((item) => item.id != id);
 
+  // add transaction to local storage
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
   // calculate Totals
   calculateTotals();
 
   addTransactionToDOM(transactions);
+};
+
+const editTransaction = (id) => {
+  // change the boolean edit variable
+  isEditing = true;
+  // find the element to edit and store it in itemToEdit
+  itemToEdit = transactions.find((item) => item.id == id);
+
+  // add the selected item info to input elements
+  transactionEl.value = itemToEdit.transaction;
+  amountEl.value = itemToEdit.amount;
+
+  // change the text of button element to edit transaction
+  btnSubmitEl.innerText = `Submit Transaction`;
 };
 
 const calculateTotals = () => {
@@ -85,13 +117,33 @@ formEl.addEventListener("submit", (event) => {
   const amount = Number(amountEl.value.trim());
 
   if (transaction && amount) {
-    const newTransaction = {
-      id: `${Date.now()}`,
-      transaction: transaction,
-      amount: amount,
-    };
+    if (isEditing) {
+      // edit the element and store it in the transaction
+      transactions = transactions.map((item) => {
+        if (item.id == itemToEdit.id) {
+          return { ...item, transaction: transaction, amount: amount };
+        } else {
+          return item;
+        }
+      });
 
-    transactions.push(newTransaction);
+      // update the helper variables
+      isEditing = false;
+      itemToEdit = {};
+
+      // update the button text
+      btnSubmitEl.innerText = `Add Transaction`;
+    } else {
+      const newTransaction = {
+        id: `${Date.now()}`,
+        transaction: transaction,
+        amount: amount,
+      };
+      transactions.push(newTransaction);
+    }
+
+    // add transaction to local storage
+    localStorage.setItem("transactions", JSON.stringify(transactions));
 
     // calculate Totals
     calculateTotals();
